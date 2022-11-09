77359010637c34cbc67cc0fed8596a2f3ffe9b67
ThiagoBrunoPereira <thiagomeezz@gmail.com>
2022-11-09 15:15:26 -0300

    Cria botões de testes

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,15 @@
   <main>
     <MyButton label="Hello" />
     <x-btn
-      color="accepted"
-      class="ml-3 mr-3 mb-3 mb-lg-0"
+      color="blue-100"
+      class="ml-3 mr-3 mb-3 mb-lg-00"
       label="Deferir pré-matrícula"
     />
+    <button type="text" class="x-btn x-btn-blue-100">teste</button>
+
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   </main>
 </template>
 
